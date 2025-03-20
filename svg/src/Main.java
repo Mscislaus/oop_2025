66cf5067206ddb9c2e248b8e074fccf7b2175e43
Yevhenii <bubkaogo@gmail.com>
2025-03-20 16:08:52 +0100

    Commit #1

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -60,17 +60,6 @@ public class Main {
         System.out.println(ellipse);
         scene.addShape(ellipse);
 
-        /* TODO: klasa Text dziedzicząca po Shape
-        *  - String text
-        *  - x, y (lewy górny róg, może być Point)
-        *  - textLength (przy okazji szerokość obiektu do BoundingBox)
-        *  - fontSize (przy okazji wysokość obiektu)
-        *  + konstruktor
-        *  + toSvg()
-        *    - atrybuty: x, y, textLength, font-size, style (z klasy Style)
-        *  + boundingBox()
-        */
-
         System.out.println(polyCopy.boundingBox());
         scene.save("rysunek.svg");
     }
