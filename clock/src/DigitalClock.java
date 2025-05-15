d9dffec85e9bcf4bb2c4abe39142af45a7d6a751
Mateusz <matter.exe@gmail.com>
2025-05-15 22:13:49 +0200

    Zegar

--- clock/src/DigitalClock.java
+++ clock/src/DigitalClock.java
@@ -13,11 +13,12 @@ public class DigitalClock extends Clock {
 
     @Override
     public String toString() {
+        DateTimeFormatter formatter;
         if (mode == Mode.H24) {
-            return super.toString();
+            formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         } else {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm:ss a");
-            return time.format(formatter);
+            formatter = DateTimeFormatter.ofPattern("h:mm:ss a");
         }
+        return time.format(formatter);
     }
 }
--- clock/src/Main.java
+++ clock/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        DigitalClock clock24 = new DigitalClock(DigitalClock.Mode.H24);
+        clock24.setTime(14, 30, 15);
+        System.out.println("Zegar 24h: " + clock24);
 
+        DigitalClock clock12 = new DigitalClock(DigitalClock.Mode.H12);
+        clock12.setTime(14, 30, 15);
+        System.out.println("Zegar 12h: " + clock12);
+
+        DigitalClock currentClock24 = new DigitalClock(DigitalClock.Mode.H24);
+        currentClock24.setCurrentTime();
+        System.out.println("Zegar bieżący (24h): " + currentClock24);
+
+        DigitalClock currentClock12 = new DigitalClock(DigitalClock.Mode.H12);
+        currentClock12.setCurrentTime();
+        System.out.println("Zegar bieżący (12h): " + currentClock12);
     }
 }
